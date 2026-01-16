3e76c0b6966e6e2b2546b492a620acd42a90bf45
boom-short <githubtutorialapp@gmail.com>
2026-01-16 22:56:02 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json';
+
+let state = {
+  market: [],
+  bigHistory: [], // এখানে সবসময় ৫০০ ডাটা থাকবে
+  predictions: [],
+  lastPeriod: null,
+  lossStreak: 0,
+  currentTab: 'mine',
+  stats: { w: 0, l: 0 },
+  confidence: 0
+};
